a14219dc6d63c171ead5539a20b9e926ab95d2f3
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-03-07 12:21:53 +0530

    [Vivek Machade] Add . Implemented two way data binding for user input in App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
  imagePath = 'assets/Logo.png';
+ userName: string = '';
  openBridgeLabz() {
   window.open('https://www.bridgelabz.com', '_blank');
 }
